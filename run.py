b18136d947599ad19b90543241d7a724133dab55
lokeshkumardevg <dev.clientg@gmail.com>
2026-04-29 00:09:43 +0530

    first time deployement

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ app = create_app()
 
 if __name__ == "__main__":
     # Use socketio.run() instead of app.run() to enable WebSocket support
-    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
+    socketio.run(app, host="0.0.0.0", port=8000, debug=True)
